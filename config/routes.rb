b660075e3dc62391d10b621556dcebddd7f6b987
dirtyhero <takahashi@temona.co.jp>
2018-09-09 13:57:21 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'rooms/show'
   get 'chat/show'
+  root 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end
